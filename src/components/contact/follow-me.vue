88c1b341f907bff1b120aece7a9dfb13a9130949
Matthew Flynn <mmflynn95@gmail.com>
2025-08-08 20:05:34 +0100

    refactor: move social media links into separate file

--- src/components/contact/follow-me.vue
+++ src/components/contact/follow-me.vue
@@ -1,33 +1,6 @@
 <script setup lang="ts">
 import ButtonWrapper from '@/components/ui/button-wrapper.vue'
-import IconStrava from '@/components/icons/icon-strava.vue'
-import IconGoodreads from '@/components/icons/icon-goodreads.vue'
-import IconLinkedIn from '@/components/icons/icon-linked-in.vue'
-import IconGithub from '@/components/icons/icon-github.vue'
-import IconBluesky from '@/components/icons/icon-bluesky.vue'
-
-const links = [
-  {
-    component: IconGithub,
-    to: 'https://github.com/matthew-flynn',
-  },
-  {
-    component: IconLinkedIn,
-    to: 'https://www.linkedin.com/in/matthew-flynn-712942113/',
-  },
-  {
-    component: IconStrava,
-    to: 'https://www.strava.com/athletes/38051091',
-  },
-  {
-    component: IconBluesky,
-    to: 'https://bsky.app/profile/matthew-flynn.bsky.social',
-  },
-  {
-    component: IconGoodreads,
-    to: 'https://www.goodreads.com/user/show/55013361-matt',
-  },
-]
+import { links } from '@/data/social-media'
 </script>
 
 <template>
