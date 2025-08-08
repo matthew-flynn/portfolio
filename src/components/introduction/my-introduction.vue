e3342a115a46a92643a110c49bafc80d44af804a
Matthew Flynn <mmflynn95@gmail.com>
2025-08-08 19:52:41 +0100

    feat: update introduction

--- src/components/introduction/my-introduction.vue
+++ src/components/introduction/my-introduction.vue
@@ -1,12 +1,22 @@
+<script setup lang="ts">
+import IntroductionParagraph from './introduction-paragraph.vue'
+</script>
+
 <template>
   <div
     class="bg-beige-500 max-w-[1440px] min-w-[320px] mx-auto px-6 md:px-24 pb-10"
   >
-    <p>Hello, my name is Matt.</p>
-    <p>
-      I'm a full stack web development team leader based in the UK with 8 years
-      of full stack software development experience - mostly using .NET and
-      Vue.js.
-    </p>
+    <introduction-paragraph>Hello, my name is Matt.</introduction-paragraph>
+    <introduction-paragraph>
+      I am currently a full stack web development team leader based in the UK
+      with 8 years of full stack web software development experience - mostly
+      using .NET and Vue.js. I am responsible for the creation and maintainance
+      of websites that are used by pharamaceutical companies worldwide.
+    </introduction-paragraph>
+    <introduction-paragraph>
+      Outside of work I can be found playing guitar, reading fantasy novels,
+      playing video games, and dipping in and out of swimming, cycling, and
+      running.
+    </introduction-paragraph>
   </div>
 </template>
