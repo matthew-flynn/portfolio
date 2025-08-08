4b2023b88480a528cef288bfce7faa1d74daa029
Matthew Flynn <mmflynn95@gmail.com>
2025-08-08 20:02:35 +0100

    feat: add bluesky link

--- src/components/contact/follow-me.vue
+++ src/components/contact/follow-me.vue
@@ -4,6 +4,7 @@ import IconStrava from '@/components/icons/icon-strava.vue'
 import IconGoodreads from '@/components/icons/icon-goodreads.vue'
 import IconLinkedIn from '@/components/icons/icon-linked-in.vue'
 import IconGithub from '@/components/icons/icon-github.vue'
+import IconBluesky from '@/components/icons/icon-bluesky.vue'
 
 const links = [
   {
@@ -17,6 +18,10 @@ const links = [
   {
     component: IconStrava,
     to: 'https://www.strava.com/athletes/38051091',
+  },
+  {
+    component: IconBluesky,
+    to: 'https://bsky.app/profile/matthew-flynn.bsky.social',
   },
   {
     component: IconGoodreads,
